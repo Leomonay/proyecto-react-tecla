c50b1b992dec2e58f89b793d71c2049458096af7
Lmonay <leonardo.monay@gmail.com>
2023-04-16 15:36:55 -0300

    class 35: Local State

--- src/components/ToDoItem.js
+++ src/components/ToDoItem.js
@@ -1,17 +1,27 @@
 import "./ToDoItem.css";
 
 export default function ToDoItem(props) {
+  const onComplete = () => {
+    alert(`Has completado: ${props.text}`);
+  };
+  const onDelete = () => {
+    alert(`Has eliminado: ${props.text}`);
+  };
+
   return (
     <li className="ToDoItem">
       <span
         className={`Icon Icon-check ${props.completed && "Icon-check--active"}`}
+        onClick={onComplete}
       >
         ✔
       </span>
       <p className={`TodoItem-p ${props.completed && "TodoItem-p--complete"}`}>
         {props.text}
       </p>
-      <span className="Icon Icon-delete">✘</span>
+      <span className="Icon Icon-delete" onClick={onDelete}>
+        ✘
+      </span>
     </li>
   );
 }
--- src/components/ToDoSearch.js
+++ src/components/ToDoSearch.js
@@ -1,14 +1,35 @@
+import { useState } from "react";
 import "./ToDoSearch.css";
 
+// class Componente extends React.Component {
+//   constructor() {
+//     this.state = {
+//       username: "Leo Monay",
+//     };
+//   }
+
+//   render() {
+//     return (
+//       <div onClick={() => this.setState({ userName: "Leonardo Monay" })}>
+//         {this.state.username}
+//       </div>
+//     );
+//   }
+// }
+
 export default function ToDoSearch() {
-  const handleChange = (e) => {
-    console.log(e.target.value);
+  const [keyWord, setKeyWord] = useState("");
+
+  const onSearchTodoValue = (event) => {
+    console.log(event.target.value);
+    setKeyWord(event.target.value);
   };
+
   return (
     <input
-      onChange={handleChange}
       className="ToDoSearch"
       placeholder="Ingresa palabra clave"
+      onChange={onSearchTodoValue}
     />
   );
 }
